828f20e3ea2372caa1f21165b47830353f109cc7
EnesDemir143 <enesd41@outlook.com>
2025-05-08 14:50:25 +0300

    Word2vec modeli classa ayarlanıp tamamlanı.

--- Pipeline/TextRepresentation/Word2vec.py
+++ Pipeline/TextRepresentation/Word2vec.py
@@ -1,10 +1,13 @@
 import gensim
+import numpy as np
+from gensim.models import KeyedVectors
+
 
 class Word2vec:
     def __init__(self):
-        self.word2vec = gensim.models.Word2Vec(
-            window=10,
-            min_count=2,
-            workers=6
-        )
+        self.word2vec = KeyedVectors.load_word2vec_format('GoogleNews-vectors-negative300.bin', binary=True)
 
+    def word_to_vector(self, text):
+        vectors = [self.word2vec[token] if token in self.word2vec.key_to_index else np.zeros(self.word2vec.vector_size)
+                   for token in text.split()]
+        return vectors
